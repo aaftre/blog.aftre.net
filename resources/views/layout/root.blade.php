c5a4f8c35eed3d4fd528e957ca598cac1e16a63d
Adrian Maliszewski <adrian@aftre.net>
2023-09-10 01:15:15 -0400

    commit 4

--- resources/views/layout/root.blade.php
+++ resources/views/layout/root.blade.php
@@ -4,27 +4,43 @@
 
 @include('partials.header')
 
-<div class="md:flex-row md:flex-wrap md:flex-border-2 border-green-500 md:mx-auto max-w-3xl object-top flex">
+<div class="md:flex-row md:flex-wrap md:flex-border-2 border-green-500 md:mx-auto max-w-5xl object-top flex">
     <div class="border-2 border-yellow-500 w-full h-28">
-        asdas
     </div>
 </div>
 
-<div class="md:flex-row md:flex-wrap md:flex-border-2 border-green-500 md:mx-auto max-w-3xl object-top flex">
+<div class="md:flex-row md:flex-wrap md:flex-border-2 border-green-500 md:mx-auto max-w-5xl object-top flex">
 
     <div class="flex-initial border-2 border-violet-500 w-1/4">
         <span>Resources</span>
     </div>
 
-    <div class="border-2 border-pink-500 bg-lime-500 w-3/4"> 
+    <div class="border-2 border-pink-500 w-3/4"> 
     @foreach($cleaver as $post)
         @if($post->path == '/') @continue @endif
 
-        <p class="text-3xs md:text-base">{!! $post->date !!}</p> 
-        <a class="text-xl" href="{!! $post->path !!}">{!! $post->title !!}</a>
-        <div>
-        {!! $post->description !!}
-        </div>
+        <header>
+            <span class="text-3xs font-body">
+                <time datetime="{!! $post->date !!}">{!! date('j M Y', strtotime($post->date)) !!}</time>
+            </span>
+            <h2 class="text-2xl font-body">
+                <a href="{!! $post->path !!}">{!! $post->title !!}</a>
+            </h2>
+        </header>
+
+        @foreach($post->sections as $section)
+            @foreach($section->contents as $content)
+                <section>
+                @if($content->type === 'code')
+                    <div class="container bg-gray-800 rounded-none md:rounded-lg mx-auto my-6 py-6 px-6 md:px-12 shadow-xl">
+                        <pre class="text-white whitespace-pre-wrap">{!! $content->text !!}</pre>
+                    </div>
+                @else
+                    <p class="my-6">{!! $content->text !!}</p>
+                @endif
+                </section>
+            @endforeach
+        @endforeach
     @endforeach
     </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,34 @@
 module.exports = {
-    purge: [
+    content: [
         './resources/views/**/*.php',
         './resources/views/**/*.html',
         './resources/assets/js/**/*.vue',
         './resources/content/**/*.html'
     ],
-    darkMode: false,
     theme: {
+        fontSize: {
+            '2xl': ['32px', {
+                lineHeight: '39px',
+                letterSpacing: '-0.01em',
+                fontWeight: '800',
+            }],
+            '3xl': ['1.875rem', {
+                lineHeight: '2.25rem',
+                letterSpacing: '-0.02em',
+                fontWeight: '700',
+            }],
+        },
+        fontFamily: {
+            'sans': ['ui-sans-serif', 'system-ui'],
+            'serif': ['ui-serif', 'Georgia'],
+            'mono': ['ui-monospace', 'SFMono-Regular'],
+            'display': ['Oswald'],
+            'body': ['"Open Sans"']
+        },
         extend: {
             fontSize: {
                 '2xs': '0.625rem', // Add a smaller text size
-                '3xs': '0.5rem',   // Add an even smaller text size
+                '3xs': '14px',   // Add an even smaller text size
                 '4xs': '0.25rem',   // Add an even smaller text size
             },
         },
